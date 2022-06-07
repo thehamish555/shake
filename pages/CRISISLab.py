98987493923bd0f84a41307d0067bd49f32f5d62
Hamish Lester <hamishlester555@gmail.com>
2022-06-07 23:11:55 +1200

    Update

---
+++ pages/CRISISLab.py
@@ -0,0 +1,17 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title='Raspberry Shake App',
+    page_icon='🤖',
+    layout='wide',
+    menu_items={
+    'Get Help': None,
+    'About': '# Raspberry Shake App',
+    'Report a bug': None}
+    )
+
+lc, mc, rc = st.columns([4, 1, 2])
+
+rc.image(Image.open('crisis_logo.png'), width=250, caption='CRISISLab Logo')
+
+st.title('CRISISLab')
--- pages/Jackbord.py
+++ pages/Jackbord.py
@@ -13,7 +13,7 @@ st.set_page_config(
 
 lc, mc, rc = st.columns([4, 1, 2])
 
-rc.image(Image.open('logo.png'), width=250, caption='Jackbord Logo')
+rc.image(Image.open('jackbord_logo.png'), width=250, caption='Jackbord Logo')
 
 lc.title('Jackbord')
 lc.write('Jackbord is the main supporter for this project at Paraparaumu '
