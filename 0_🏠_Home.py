d81493fc138a2a6eadfce790889e9578a4bc0e51
Hamish Lester <hamishlester555@gmail.com>
2022-06-09 14:23:41 +1200

    Update

--- 0_🏠_Home.py
+++ 0_🏠_Home.py
@@ -1,4 +1,5 @@
 import streamlit as st
+from PIL import Image
 
 st.set_page_config(
     page_title='Raspberry Shake App',
@@ -10,7 +11,21 @@ st.set_page_config(
     'Report a bug': None}
     )
 
-lc, rc = st.columns(2)
+lc, mc, rc = st.columns([4, 1, 2])
 
-lc.title('Raspberry Shake Data')
-lc.write('This app is designed to display data for the Raspberry Shake')
+rc.image(Image.open('./media/shake_logo.png'), width=350)
+rc.image(Image.open('./media/crisislab_logo.png'), width=200)
+rc.image(Image.open('./media/massey_logo.png'), width=350)
+rc.image(Image.open('./media/jackbord_logo.png'), width=150)
+rc.image(Image.open('./media/paraparaumu_logo.png'), width=350)
+
+rc.markdown('<img src="/media/shake_logo.png" style="width: 350px">', unsafe_allow_html=True)
+rc.markdown('<img src="/media/crisislab_logo.png" style="width: 200px">', unsafe_allow_html=True)
+rc.markdown('<img src="/media/massey_logo.png" style="width: 350px">', unsafe_allow_html=True)
+rc.markdown('<img src="/media/jackbord_logo.png" style="width: 150px">', unsafe_allow_html=True)
+rc.markdown('<img src="/media/paraparaumu_logo.png" style="width: 350px">', unsafe_allow_html=True)
+
+lc.title('Raspberry Shake App')
+lc.write('This app is designed to display data for the Raspberry Shake for '
+    'the [CRISiSLab](/CRISiSLab). You can find information on the supporters '
+    'and the competition using the navigation menu to the left.')
